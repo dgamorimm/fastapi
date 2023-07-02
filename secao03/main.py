de50112416d1359428f8a9c39ec1f9ccea5cf2a4
Douglas Amorim <douglas_amorimm@outlook.com>
2023-07-02 13:12:53 -0300

    docs

--- secao03/main.py
+++ secao03/main.py
@@ -1,4 +1,4 @@
-from typing import Any, Optional
+from typing import List, Dict, Any, Optional
 
 from fastapi import FastAPI
 from fastapi import HTTPException
@@ -13,6 +13,7 @@ from fastapi import Depends
 from time import sleep
 
 from models import Curso
+from models import cursos
 
 def fake_db():
     try:
@@ -22,30 +23,33 @@ def fake_db():
         print('Fechando conexão com banco de dados')
         sleep(1)
 
-app = FastAPI()
+# adiciona o titula na documentação
+# adiciona a versão na documentação
+app = FastAPI(
+    title='API de cursos da Geek University',
+    version='0.0.1',
+    description='Uma API para estudo do FastAPI'
+)
 
-cursos = {
-    1: {
-        'titulo': 'Programação para Leigos',
-        'aulas' : 112,
-        'horas' : 58
-    },
-    2: {
-        'titulo': 'Algoritmos e Lógica de Programação',
-        'aulas' : 87,
-        'horas' : 67
-    }
-}
-
-@app.get('/cursos')
+# description e summary serve para a documentação
+# response_model também serve para colocar o que vai retornar na documentação
+# response_description retorna a mensagem de sucesso
+@app.get('/cursos', 
+         description='Retorna todos os cursos ou uma lista vazia', 
+         summary='Retorna todos os cursos',
+         response_model=List[Curso],
+         response_description='Cursos encontrados com sucesso')
 async def get_cursos(db:Any = Depends(fake_db)):
     return cursos
 
-@app.get('/cursos/{curso_id}')
-async def get_cursos(curso_id : int = Path(title='ID do curso',description='Deve ser entre 1 e 2',gt=0,lt=3), db:Any = Depends(fake_db)):  # ele trasforma para o tipo inteiro através do type hint
+@app.get('/cursos/{curso_id}',
+         description='Retorna um curso em um dicionário', 
+         summary='Retorna um único curso',
+         response_model=Curso)
+async def get_cursos(curso_id : int = Path(title='ID do curso',description='Deve ser entre 1 e 4',gt=0,lt=5), db:Any = Depends(fake_db)):  # ele trasforma para o tipo inteiro através do type hint
     # tratando as exceções caso não exista o curso
     try:
-        curso = cursos[curso_id]
+        curso = cursos[curso_id - 1]
         return curso
     except KeyError:
         raise HTTPException(
@@ -53,7 +57,9 @@ async def get_cursos(curso_id : int = Path(title='ID do curso',description='Deve
             detail='Curso não encontrado'
         )
 
-@app.post('/cursos', status_code=status.HTTP_201_CREATED)
+@app.post('/cursos', 
+          status_code=status.HTTP_201_CREATED,
+          response_model=Curso)
 async def post_curso(curso:Curso, db:Any = Depends(fake_db)):
     next_id: int = len(cursos) + 1
     cursos[next_id] = curso
@@ -63,7 +69,7 @@ async def post_curso(curso:Curso, db:Any = Depends(fake_db)):
 @app.put('/cursos/{curso_id}')
 async def put_curso(curso_id:int, curso:Curso, db:Any = Depends(fake_db)):
     if curso_id in cursos:
-        cursos[curso_id] = curso
+        cursos[curso_id - 1] = curso
         del curso.id
         return curso
     else:
@@ -75,7 +81,7 @@ async def put_curso(curso_id:int, curso:Curso, db:Any = Depends(fake_db)):
 @app.delete('/cursos/{curso_id}')
 async def delete_curso(curso_id:int, db:Any = Depends(fake_db)):
     if curso_id in cursos:
-        del cursos[curso_id]
+        del cursos[curso_id - 1]
         return Response(
             status_code=status.HTTP_204_NO_CONTENT
         )
--- secao03/models.py
+++ secao03/models.py
@@ -6,3 +6,10 @@ class Curso(BaseModel):
     titulo: str
     aulas:  int
     horas:  int
+
+cursos = [
+    Curso(id=1, titulo='Programação para Leigos', aulas=42, horas=56),
+    Curso(id=2, titulo='Programação com Javascript', aulas=38, horas=99),
+    Curso(id=3, titulo='Programação Front com React', aulas=71, horas=22),
+    Curso(id=4, titulo='React Native Patterns', aulas=89, horas=120),
+]

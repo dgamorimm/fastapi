f1f83175dfa0974f7aa0b8fadbb06ec41c579066
Douglas Amorim <douglas_amorimm@outlook.com>
2023-07-03 13:22:04 -0300

    primeira versao sql alchemy

---
+++ secao04/core/configs.py
@@ -0,0 +1,18 @@
+# configurações gerais que vamos usar em todo o projeto
+
+from os import getenv
+from pydantic import BaseSettings
+from sqlalchemy.ext.declarative import declarative_base
+
+class Settings(BaseSettings):
+    """
+        Configurações gerais usadas na aplicação
+    """
+    API_V1_STR:str = '/api/v1'
+    DB_URL:str = f'postgresql+asyncpg://{getenv("PGSQL_USERNAME")}:{getenv("PGSQL_PASSWORD")}@localhost:5432/faculdade'
+    DBBaseModel = declarative_base()
+    
+    class Config:
+        case_sensitive = True
+        
+settings = Settings()
